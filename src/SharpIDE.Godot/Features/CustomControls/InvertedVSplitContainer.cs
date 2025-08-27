cff88764462a220ff6c5edf744985770b1f70c24
Matt Parker <61717342+MattParkerDev@users.noreply.github.com>
2025-08-27 19:48:09 +1000

    Create InvertedVSplitContainer node type

---
+++ src/SharpIDE.Godot/Features/CustomControls/InvertedVSplitContainer.cs
@@ -0,0 +1,28 @@
+﻿using Godot;
+
+namespace SharpIDE.Godot.Features.CustomControls;
+
+[GlobalClass, Tool]
+public partial class InvertedVSplitContainer : VSplitContainer
+{
+    [Export]
+    private int _invertedOffset = 200;
+
+    public override void _Ready()
+    {
+        Dragged += OnDragged;
+    }
+
+    private void OnDragged(long offset)
+    {
+        _invertedOffset = (int)Size.Y - SplitOffset;
+    }
+    
+    public override void _Notification(int what)
+    {
+        if (what == NotificationResized)
+        {
+            SplitOffset = (int)Size.Y - _invertedOffset;
+        }
+    }
+}
