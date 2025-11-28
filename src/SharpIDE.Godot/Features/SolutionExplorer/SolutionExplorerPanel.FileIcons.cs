cfc753874d56ff3edc16565990201d518cf407de
Matt Parker <61717342+MattParkerDev@users.noreply.github.com>
2025-11-29 00:08:01 +1000

    add html icon

--- src/SharpIDE.Godot/Features/SolutionExplorer/SolutionExplorerPanel.FileIcons.cs
+++ src/SharpIDE.Godot/Features/SolutionExplorer/SolutionExplorerPanel.FileIcons.cs
@@ -9,6 +9,7 @@ public partial class SolutionExplorerPanel
     private readonly Texture2D _razorIcon = ResourceLoader.Load<Texture2D>("uid://cff7jlvj2tlg2");
     private readonly Texture2D _jsonIcon = ResourceLoader.Load<Texture2D>("uid://csrwpjk77r731");
     private readonly Texture2D _jsIcon = ResourceLoader.Load<Texture2D>("uid://cpdobpjrm2suc");
+    private readonly Texture2D _htmlIcon = ResourceLoader.Load<Texture2D>("uid://q0cktiwdkt1e");
     private readonly Texture2D _txtIcon = ResourceLoader.Load<Texture2D>("uid://b6bpjhs2o1j2l");
 
     private Texture2D GetIconForFileExtension(string fileExtension)
@@ -19,6 +20,7 @@ public partial class SolutionExplorerPanel
             ".razor" or ".cshtml" => _razorIcon,
             ".json" => _jsonIcon,
             ".js" => _jsIcon,
+            ".html" or ".htm" => _htmlIcon,
             ".txt" => _txtIcon,
             _ => _csIcon
         };    
